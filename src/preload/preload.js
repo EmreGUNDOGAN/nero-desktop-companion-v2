7cb0fad7805ba22c6a9cc6f3eb6345537515d23a
Emre Gündoğan <emregundogan5@gmail.com>
2026-09-23 15:25:25 +0300

    feat: allow moodboard history fetch IPC

--- src/preload/preload.js
+++ src/preload/preload.js
@@ -6,7 +6,7 @@ const INVOKE = new Set([
   'state:get', 'theme:get',
   'notes:save', 'notes:delete', 'notes:archive',
   'todos:add', 'todos:toggle', 'todos:rename', 'todos:delete', 'todos:clearDone', 'todos:archive', 'todos:archiveDone', 'todos:setReminder',
-  'todos:stopwatchStart', 'todos:stopwatchPause', 'jar:add', 'moodboard:set',
+  'todos:stopwatchStart', 'todos:stopwatchPause', 'jar:add', 'moodboard:get', 'moodboard:set',
   'data:export', 'data:import', 'data:openBackups',
   'update:check', 'update:install', 'update:onQuit', 'update:dismiss',
   'timer:start', 'timer:pause', 'timer:resume', 'timer:cancel', 'timer:badge',
